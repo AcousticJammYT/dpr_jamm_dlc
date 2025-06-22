30ab12e76e44ef7fee7f86e09558442ae61dc452
AcousticJamm <jasmicheals@gmail.com>
2025-06-22 17:31:45 -0400

    Row, row, row your boat, sail across the sea
    Heave 'er up and shake the dust, the pain is but a dream

--- scripts/hooks/PartyBattler.lua
+++ scripts/hooks/PartyBattler.lua
@@ -1,16 +1,5 @@
 local PartyBattler, super = Class(PartyBattler)
 
-function PartyBattler:init(chara, x, y)
-    super.init(self, chara, x, y)
-	
-	self.doopShader = Assets.newShader("doop")
-	
-	if Game:getFlag("should_doop") and self.chara.id == Game.party[1].id then
-		doop_fx = ShaderFX(self.doopShader, {})
-		self:addFX(doop_fx, "doop_fx")
-	end
-end
-
 function PartyBattler:doOverlay()
 	if (not self.defending) and (not self.is_down) then
 		if not (Game.battle.soul and Game.battle.soul.FS) then
